2cf5d63652ffbd57bc0f085d206248de5b09ed69
marufsiyam91 <marufsiyam9123@gmail.com>
2024-10-23 22:21:24 +0600

    add java array workplace

---
+++ src/com/Array/Array.java
@@ -0,0 +1,17 @@
+package com.Array;
+
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] Array = new int[5];
+
+        Array[0] = 25;
+        Array[1] = 24;
+        Array[2] = 4;
+        Array[3] = 2;
+        Array[4] = 0;
+
+        System.out.println(Arrays.toString(Array));
+    }
+        }
