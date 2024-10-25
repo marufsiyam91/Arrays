fd5f7fd52ae97fc27cb4f33ca3495f8a48bc4076
marufsiyam91 <marufsiyam9123@gmail.com>
2024-10-25 21:25:48 +0600

    2 dimentional array or nested array input through loop

--- src/com/Array/Array2d.java
+++ src/com/Array/Array2d.java
@@ -9,13 +9,21 @@ public class Array2d {
 
         Scanner input = new Scanner(System.in);
 
-        int[] Array = new int[10];
+//        int[] Array = new int[10];
+        int[][] Array2d = new int[3][3];
 
-        for(int i = 0; i < Array.length; i++){
-            Array[i] = input.nextInt();
+//        for(int i = 0; i < Array.length; i++){
+//            Array[i] = input.nextInt();
+//        }
+
+        for(int rows = 0; rows < Array2d.length; rows++){
+            for(int values = 0; values < Array2d[rows].length; values++){
+                Array2d[rows][values] = input.nextInt();
+            }
         }
 
-        System.out.println(Arrays.toString(Array));
+//        System.out.println(Arrays.toString(Array));
+        System.out.println(Arrays.toString(Array2d[2]));
 
     }
 }
